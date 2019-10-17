3b3a79a398fa2f1d1dc792a8df4c55a085f2048c
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-10-17 09:55:35 +0200

    Add actions.js

---
+++ actions.js
@@ -0,0 +1,44 @@
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+export function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+export function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+export function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+
+export function thumbUpComment(id, votes) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        votes
+    }
+}
+
+export function thumbDownComment(id, votes ) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        votes
+    }
+}
