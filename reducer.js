bdac4373f8e35f5df7ab97cabfe969a94fcdd0a7
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-10-18 12:09:21 +0200

    Modify reducer, Add comments.js

--- reducer.js
+++ reducer.js
@@ -1,64 +1,10 @@
-import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
-const initialState = {
-    comments: [],
-    users: []
-};
+const app = combineReducers({
+    comments,
+    users
+});
 
-export default function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                    votes: 0
-                }
-                , ...state.comments]
-            });
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
-        case EDIT_COMMENT: 
-            return Object.assign({}, state, {
-                comments:  state.comments.map(comment => {
-                    if (comment.id === action.id) {
-                        return {
-                            ...comment,
-                            text : action.text
-                        }
-                    }
-                    return comment;
-                })
-            });
-        case THUMB_UP_COMMENT: 
-            return Object.assign({}, state, {
-                comments: state.comments.map(comment => {
-                    if (comment.id === action.id) {
-                        return {
-                            ...comment,
-                             votes: action.votes++ 
-                            }
-                    }  
-                    return comment;
-                })
-            });
-        case THUMB_DOWN_COMMENT: 
-            return Object.assign({}, state, {
-                comments: state.comments.map(comment => {
-                    if (comment.id === action.id) {
-                        return {
-                            ...comment,
-                            votes: action.votes --
-                        }
-                    } 
-                    return comment ;
-                })
-            });
 
-        default:
-            return state;
-    }
-}
